8580075e083cc4e51a2004177ed4cacf57543ffc
nilewapp <robert.welin@gmail.com>
2013-09-10 20:20:51 +0100

    Records key events

--- src-cljs/type_rec/main.cljs
+++ src-cljs/type_rec/main.cljs
@@ -1,15 +1,25 @@
-;; Copyright (c) 2013, Robert welin
-(ns type-rec.hello
-  (:use [domina :only [by-id value set-value! set-text!]]))
+; Copyright (c) 2013, Robert welin
+(ns type-rec.main
+  (:require [domina :refer [by-id value set-text! append! text]]
+            [domina.events :refer [listen! prevent-default]]))
 
-(defn show-cursor-position []
+(def tick 1000)
+
+(def key-events [])
+
+(defn show-data []
   (let [cursor-position (.-selectionStart (by-id "editor"))]
-    (set-text! (by-id "cursor-position") cursor-position)))
+    (do (set-text! (by-id "cursor-position") cursor-position)
+        (set-text! (by-id "current-input") key-events))))
+
+(defn record-key-event [evt]
+  (set! key-events (conj key-events (:charCode evt))))
 
 (defn init []
   (if (and js/document
            (.-getElementById js/document))
     (let [start-button (.getElementById js/document "start")]
-      (js/setInterval show-cursor-position 1000))))
+      (do (js/setInterval show-data tick)
+          (listen! (by-id "editor") :keypress (fn [evt] (record-key-event evt)))))))
 
 (set! (.-onload js/window) init)
