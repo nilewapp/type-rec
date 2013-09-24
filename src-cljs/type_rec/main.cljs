bade77bf01b91367127783c8498db48c3b335d5a
nilewapp <robert.welin@gmail.com>
2013-09-25 00:22:05 +0100

    All key events during a tick is stored in a buffer that at the end of the tick is appended to a main buffer and then cleared

--- src-cljs/type_rec/main.cljs
+++ src-cljs/type_rec/main.cljs
@@ -1,19 +1,27 @@
 ; Copyright (c) 2013, Robert welin
 (ns type-rec.main
-  (:require [domina :refer [by-id value set-text! append! text]]
-            [domina.events :refer [listen! prevent-default]]))
+  (:require [domina :refer [by-id value set-text! set-value! append! text]]
+            [domina.events :refer [listen! unlisten! dispatch! prevent-default]]))
 
-(def tick 1000)
+(def tick 100)
 
 (def key-events [])
 
+(def key-tick-buffer [])
+
+(defn dispatch-key [k]
+  (dispatch! (by-id "editor") :keypress {:charCode k}))
+
 (defn show-data []
   (let [cursor-position (.-selectionStart (by-id "editor"))]
-    (do (set-text! (by-id "cursor-position") cursor-position)
-        (set-text! (by-id "current-input") key-events))))
+    (do (set! key-events (conj key-events key-tick-buffer))
+        (set-text! (by-id "cursor-position") cursor-position)
+        (set-text! (by-id "current-input") key-tick-buffer)
+        ;(set-value! (by-id "editor") (apply str (map String/fromCharCode (flatten key-events))))
+        (set! key-tick-buffer []))))
 
 (defn record-key-event [evt]
-  (set! key-events (conj key-events (:charCode evt))))
+  (set! key-tick-buffer (conj key-tick-buffer (:charCode evt))))
 
 (defn init []
   (if (and js/document
